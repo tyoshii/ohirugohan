a6b7487f11c21c155447ba62d42243acb48385dc
Takanao Yoshii <tyoshii716@gmail.com>
2012-06-17 02:10:24 +0900

    init sql

---
+++ sql/init.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS ohirugohan;
+CREATE DATABASE ohirugohan;
+
+use ohirugohan;
+
+CREATE TABLE META (
+    Name    VARCHAR(128),
+    Path    VARCHAR(128),
+    Storage VARCHAR(64)
+) ENGINE=InnoDB;
+
+CREATE TABLE STORAGE (
+    Id   INTEGER UNSIGNED PRIMARY KEY,
+    Name VARCHAR(64)
+) ENGINE=InnoDB;
