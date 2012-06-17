705878939b4f0cf29e3831c5050e0d3bb7d35b27
Takanao Yoshii <tyoshii716@gmail.com>
2012-06-17 12:03:17 +0900

    database interface

--- www/system/Ohirugohan.php
+++ www/system/Ohirugohan.php
@@ -10,4 +10,16 @@ class Ohirugohan {
     require_once dirname(__FILE__)."/../model/{$name}.php";
     $this->$name = new $name();
   }
+
+  protected function database ( $key = 'common' ) {
+    require_once dirname(__FILE__)."/../config/database.php";
+
+    $dbs = sprintf( '%s:host=%s;dbname=%s',
+      $_database[$key]['driver'],
+      $_database[$key]['host'],
+      $_database[$key]['dbname']
+    );
+
+    $this->db = new PDO( $dbs, $_database[$key]['user'], $_database[$key]['passwd'] );
+  }
 }
