e52851da8c34216c668cc5dd0f0062905d1b4517
Takanao Yoshii <tyoshii716@gmail.com>
2012-06-17 12:24:17 +0900

    added column

--- sql/init.sql
+++ sql/init.sql
@@ -4,9 +4,12 @@ CREATE DATABASE ohirugohan;
 use ohirugohan;
 
 CREATE TABLE META (
-    Name    VARCHAR(128),
-    Path    VARCHAR(128),
-    Storage VARCHAR(64)
+    Id        INTEGER UNSIGNED PRIMARY KEY,
+    Name      VARCHAR(128),
+    Path      VARCHAR(128),
+    Storage   VARCHAR(64),
+    IsDelete  ENUM('y', 'n') DEFAULT 'n', 
+    UpdatedAt DATETIME 
 ) ENGINE=InnoDB;
 
 CREATE TABLE STORAGE (
